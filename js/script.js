bc641393a22016eaaa9043f482b4c45e1f1f866b
Weston Partin <djwest91@gmail.com>
2021-04-06 07:22:29 -0500

    added script to toggle mobile nav

--- js/script.js
+++ js/script.js
@@ -1,1 +1,39 @@
 "use strict";
+
+const menuBtn = document.querySelector(".menu-btn");
+const menuClose = document.querySelector(".close-btn");
+const menuOpen = document.querySelector(".menu-open");
+
+const open = () => {
+  menuBtn.classList.add("hidden");
+  menuClose.classList.remove("hidden");
+  menuOpen.classList.remove("hidden");
+  console.log("opened");
+};
+
+const close = () => {
+  menuBtn.classList.remove("hidden");
+  menuClose.classList.add("hidden");
+  menuOpen.classList.add("hidden");
+  console.log("closed");
+};
+
+menuBtn.addEventListener("touchend", (e) => {
+  e.preventDefault();
+  open();
+});
+
+menuBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  open();
+});
+
+menuClose.addEventListener("touchend", (e) => {
+  e.preventDefault();
+  close();
+});
+
+menuClose.addEventListener("click", (e) => {
+  e.preventDefault();
+  close();
+});
